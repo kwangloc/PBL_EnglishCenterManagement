23e64f7cad49c17574a8b56e0fd4d85fb1bb5d30
tvlong1903@gmail.com <tvlong1903@gmail.com>
2023-05-13 19:33:15 +0700

    Design Profile Form

--- PBL_EnglishCenter/View/profileManage.Designer.cs
+++ PBL_EnglishCenter/View/profileManage.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.lb_fullname = new System.Windows.Forms.Label();
-            this.bt_profile = new System.Windows.Forms.Button();
             this.bt_logout = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.tb_fullname = new System.Windows.Forms.TextBox();
@@ -55,27 +54,21 @@
             // lb_fullname
             // 
             this.lb_fullname.AutoSize = true;
-            this.lb_fullname.Location = new System.Drawing.Point(952, 81);
+            this.lb_fullname.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lb_fullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_fullname.Location = new System.Drawing.Point(179, 24);
+            this.lb_fullname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_fullname.Name = "lb_fullname";
-            this.lb_fullname.Size = new System.Drawing.Size(57, 16);
+            this.lb_fullname.Size = new System.Drawing.Size(126, 33);
             this.lb_fullname.TabIndex = 13;
             this.lb_fullname.Text = "fullname";
             // 
-            // bt_profile
-            // 
-            this.bt_profile.BackColor = System.Drawing.SystemColors.Highlight;
-            this.bt_profile.Location = new System.Drawing.Point(1133, 70);
-            this.bt_profile.Name = "bt_profile";
-            this.bt_profile.Size = new System.Drawing.Size(143, 39);
-            this.bt_profile.TabIndex = 10;
-            this.bt_profile.Text = "Profile";
-            this.bt_profile.UseVisualStyleBackColor = false;
-            // 
             // bt_logout
             // 
-            this.bt_logout.Location = new System.Drawing.Point(1373, 77);
+            this.bt_logout.Location = new System.Drawing.Point(1030, 63);
+            this.bt_logout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_logout.Name = "bt_logout";
-            this.bt_logout.Size = new System.Drawing.Size(143, 39);
+            this.bt_logout.Size = new System.Drawing.Size(107, 32);
             this.bt_logout.TabIndex = 11;
             this.bt_logout.Text = "Log out";
             this.bt_logout.UseVisualStyleBackColor = true;
@@ -83,122 +76,143 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(364, 194);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.Size = new System.Drawing.Size(94, 24);
             this.label1.TabIndex = 14;
             this.label1.Text = "Full name";
             // 
             // tb_fullname
             // 
-            this.tb_fullname.Location = new System.Drawing.Point(473, 192);
+            this.tb_fullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_fullname.Location = new System.Drawing.Point(120, 101);
+            this.tb_fullname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_fullname.Name = "tb_fullname";
-            this.tb_fullname.Size = new System.Drawing.Size(190, 22);
+            this.tb_fullname.Size = new System.Drawing.Size(217, 26);
             this.tb_fullname.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(364, 240);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.Size = new System.Drawing.Size(74, 24);
             this.label2.TabIndex = 14;
             this.label2.Text = "Gender";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(364, 292);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 186);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.Size = new System.Drawing.Size(66, 24);
             this.label3.TabIndex = 14;
             this.label3.Text = "Phone";
             // 
             // tb_phone
             // 
-            this.tb_phone.Location = new System.Drawing.Point(473, 290);
+            this.tb_phone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_phone.Location = new System.Drawing.Point(121, 188);
+            this.tb_phone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_phone.Name = "tb_phone";
-            this.tb_phone.Size = new System.Drawing.Size(190, 22);
+            this.tb_phone.Size = new System.Drawing.Size(217, 26);
             this.tb_phone.TabIndex = 15;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(364, 349);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 234);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.Size = new System.Drawing.Size(58, 24);
             this.label4.TabIndex = 14;
             this.label4.Text = "Gmail";
             // 
             // tb_gmail
             // 
-            this.tb_gmail.Location = new System.Drawing.Point(473, 347);
+            this.tb_gmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_gmail.Location = new System.Drawing.Point(121, 236);
+            this.tb_gmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_gmail.Name = "tb_gmail";
-            this.tb_gmail.Size = new System.Drawing.Size(190, 22);
+            this.tb_gmail.Size = new System.Drawing.Size(217, 26);
             this.tb_gmail.TabIndex = 15;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(751, 242);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(390, 143);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.Size = new System.Drawing.Size(97, 24);
             this.label5.TabIndex = 14;
             this.label5.Text = "Username";
             // 
             // tb_username
             // 
-            this.tb_username.Location = new System.Drawing.Point(883, 240);
+            this.tb_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_username.Location = new System.Drawing.Point(498, 141);
+            this.tb_username.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_username.Name = "tb_username";
             this.tb_username.ReadOnly = true;
-            this.tb_username.Size = new System.Drawing.Size(191, 22);
+            this.tb_username.Size = new System.Drawing.Size(217, 26);
             this.tb_username.TabIndex = 15;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(751, 288);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(390, 186);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(83, 20);
+            this.label6.Size = new System.Drawing.Size(92, 24);
             this.label6.TabIndex = 14;
             this.label6.Text = "Password";
             // 
             // tb_password
             // 
-            this.tb_password.Location = new System.Drawing.Point(883, 286);
+            this.tb_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_password.Location = new System.Drawing.Point(498, 186);
+            this.tb_password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_password.Name = "tb_password";
-            this.tb_password.Size = new System.Drawing.Size(191, 22);
+            this.tb_password.Size = new System.Drawing.Size(217, 26);
             this.tb_password.TabIndex = 15;
             // 
             // lb_detailsInfo
             // 
             this.lb_detailsInfo.AutoSize = true;
-            this.lb_detailsInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_detailsInfo.Location = new System.Drawing.Point(751, 349);
+            this.lb_detailsInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_detailsInfo.Location = new System.Drawing.Point(390, 236);
+            this.lb_detailsInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_detailsInfo.Name = "lb_detailsInfo";
-            this.lb_detailsInfo.Size = new System.Drawing.Size(94, 20);
+            this.lb_detailsInfo.Size = new System.Drawing.Size(100, 24);
             this.lb_detailsInfo.TabIndex = 14;
             this.lb_detailsInfo.Text = "Details Info";
             // 
             // tb_detailsInfo
             // 
-            this.tb_detailsInfo.Location = new System.Drawing.Point(883, 347);
+            this.tb_detailsInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_detailsInfo.Location = new System.Drawing.Point(498, 236);
+            this.tb_detailsInfo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_detailsInfo.Name = "tb_detailsInfo";
-            this.tb_detailsInfo.Size = new System.Drawing.Size(280, 22);
+            this.tb_detailsInfo.Size = new System.Drawing.Size(217, 26);
             this.tb_detailsInfo.TabIndex = 15;
             // 
             // rb_male
             // 
             this.rb_male.AutoSize = true;
-            this.rb_male.Location = new System.Drawing.Point(473, 239);
+            this.rb_male.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_male.Location = new System.Drawing.Point(132, 141);
+            this.rb_male.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rb_male.Name = "rb_male";
-            this.rb_male.Size = new System.Drawing.Size(58, 20);
+            this.rb_male.Size = new System.Drawing.Size(69, 28);
             this.rb_male.TabIndex = 16;
             this.rb_male.TabStop = true;
             this.rb_male.Text = "Male";
@@ -207,9 +221,11 @@
             // rb_female
             // 
             this.rb_female.AutoSize = true;
-            this.rb_female.Location = new System.Drawing.Point(582, 240);
+            this.rb_female.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_female.Location = new System.Drawing.Point(217, 139);
+            this.rb_female.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rb_female.Name = "rb_female";
-            this.rb_female.Size = new System.Drawing.Size(74, 20);
+            this.rb_female.Size = new System.Drawing.Size(92, 28);
             this.rb_female.TabIndex = 16;
             this.rb_female.TabStop = true;
             this.rb_female.Text = "Female";
@@ -217,47 +233,56 @@
             // 
             // bt_save
             // 
-            this.bt_save.Location = new System.Drawing.Point(550, 489);
+            this.bt_save.BackColor = System.Drawing.Color.SkyBlue;
+            this.bt_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.bt_save.Location = new System.Drawing.Point(179, 313);
+            this.bt_save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_save.Name = "bt_save";
-            this.bt_save.Size = new System.Drawing.Size(143, 39);
+            this.bt_save.Size = new System.Drawing.Size(130, 59);
             this.bt_save.TabIndex = 10;
             this.bt_save.Text = "Save";
-            this.bt_save.UseVisualStyleBackColor = true;
+            this.bt_save.UseVisualStyleBackColor = false;
             this.bt_save.Click += new System.EventHandler(this.bt_save_Click);
             // 
             // bt_cancel
             // 
-            this.bt_cancel.Location = new System.Drawing.Point(738, 489);
+            this.bt_cancel.BackColor = System.Drawing.Color.SkyBlue;
+            this.bt_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.bt_cancel.Location = new System.Drawing.Point(428, 313);
+            this.bt_cancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_cancel.Name = "bt_cancel";
-            this.bt_cancel.Size = new System.Drawing.Size(143, 39);
+            this.bt_cancel.Size = new System.Drawing.Size(130, 59);
             this.bt_cancel.TabIndex = 10;
             this.bt_cancel.Text = "Cancel";
-            this.bt_cancel.UseVisualStyleBackColor = true;
+            this.bt_cancel.UseVisualStyleBackColor = false;
             this.bt_cancel.Click += new System.EventHandler(this.bt_cancel_Click);
             // 
             // lb_type
             // 
             this.lb_type.AutoSize = true;
-            this.lb_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_type.Location = new System.Drawing.Point(751, 194);
+            this.lb_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_type.Location = new System.Drawing.Point(390, 101);
+            this.lb_type.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_type.Name = "lb_type";
-            this.lb_type.Size = new System.Drawing.Size(45, 20);
+            this.lb_type.Size = new System.Drawing.Size(53, 24);
             this.lb_type.TabIndex = 14;
             this.lb_type.Text = "Type";
             // 
             // tb_type
             // 
-            this.tb_type.Location = new System.Drawing.Point(883, 192);
+            this.tb_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_type.Location = new System.Drawing.Point(498, 101);
+            this.tb_type.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_type.Name = "tb_type";
             this.tb_type.ReadOnly = true;
-            this.tb_type.Size = new System.Drawing.Size(191, 22);
+            this.tb_type.Size = new System.Drawing.Size(217, 26);
             this.tb_type.TabIndex = 15;
             // 
             // profileManage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1361, 615);
+            this.ClientSize = new System.Drawing.Size(744, 408);
             this.Controls.Add(this.rb_female);
             this.Controls.Add(this.rb_male);
             this.Controls.Add(this.tb_gmail);
@@ -278,9 +303,10 @@
             this.Controls.Add(this.lb_fullname);
             this.Controls.Add(this.bt_cancel);
             this.Controls.Add(this.bt_save);
-            this.Controls.Add(this.bt_profile);
             this.Controls.Add(this.bt_logout);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "profileManage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "profileManage";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -290,7 +316,6 @@
         #endregion
 
         private System.Windows.Forms.Label lb_fullname;
-        private System.Windows.Forms.Button bt_profile;
         private System.Windows.Forms.Button bt_logout;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tb_fullname;
